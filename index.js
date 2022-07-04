1a95cf58cb24a38601fba974320db2bdad2299ba
Finn Thorwarth <finn@wg-werbeagentur.de>
2022-07-04 18:21:23 +0200

    Added SQL Querrys

--- index.js
+++ index.js
@@ -15,32 +15,18 @@ app.get('/', function (req, res) {
 })
 
 app.get('/results', (req, res) => {
-  axios(url)
-    .then(response => {
-      const html = response.data
-      const $ = cheerio.load(html)
-      const articles = []
-
-      $('h1', html).each(function () { //<-- cannot be a function expression
-        const title = $(this).text()
-        const url = $(this).find('a').attr('href')
-        articles.push({
-          title,
-          url
-        })
-      })
-      res.json(articles)
-    }).catch(err => console.log(err))
-
+  getArticles()
+  res.json('WG Webscraper')
 })
 
 //import config from './src/config.json'
 const config = require('./src/config.json')
 
 var connection = mysql.createConnection({
-  host     : config.database.host,
-  user     : config.database.user,
-  password : config.database.password
+  host: config.database.host,
+  user: config.database.user,
+  password: config.database.password,
+  database: config.database.database
 });
 
 connection.connect();
@@ -56,19 +42,39 @@ function getArticles() {
       .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
-        const articles = []
+        const data = {}
         $('h1', html).each(function () { //<-- cannot be a function expression
-          const title = $(this).text()
-          const url = $(this).find('a').attr('href')
-          articles.push({
-            title,
-            url
-          })
+          data.title = $(this).text()
+          data.name = url.name
+          data.url = url.url
         })
-        console.log(articles)
+        console.log(data)
+        console.log(url.name)
+        // safe to database
+        safeToDatabase(data);
+
       }).catch(err => console.log(err))
   })
 }
+
+// safe to database
+function safeToDatabase(data) {
+  console.log(data.name);
+  // check if data is already in database
+  connection.query('SELECT * FROM Projekte WHERE Name = ' + mysql.escape(data.name), function (err, result) {
+    if (err) throw err;
+    if (result.length === 0) {
+      // insert data into database with mysql
+      connection.query('INSERT INTO Projekte (Name,URL) VALUES ('+ mysql.escape(data.name)+','+mysql.escape(data.url)+')' , function (err, result) {
+        if (err) throw err;
+        console.log('1 record inserted');
+      });
+    } else {
+      console.log('data already in database')
+    }
+  });
+}
+
 // run function every 6 hours
 setInterval(() => {
   getArticles()
