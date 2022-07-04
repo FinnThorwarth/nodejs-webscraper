d4d731a0f35568547cb4fbd00a0d7287d600a939
Finn Thorwarth <finn@wg-werbeagentur.de>
2022-07-04 17:33:19 +0200

    Update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const PORT = 8080
 const axios = require('axios')
 const cheerio = require('cheerio')
 const express = require('express')
+var mysql = require('mysql');
 const app = express()
 const cors = require('cors')
 app.use(cors())
@@ -9,31 +10,70 @@ app.use(cors())
 const url = 'https://www.wg-werbeagentur.de'
 
 
-
-
 app.get('/', function (req, res) {
-    res.json('This is my webscraper')
+  res.json('WG Webscraper')
 })
 
 app.get('/results', (req, res) => {
-    axios(url)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const articles = []
+  axios(url)
+    .then(response => {
+      const html = response.data
+      const $ = cheerio.load(html)
+      const articles = []
 
-            $('h1', html).each(function () { //<-- cannot be a function expression
-                const title = $(this).text()
-                const url = $(this).find('a').attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            res.json(articles)
-        }).catch(err => console.log(err))
+      $('h1', html).each(function () { //<-- cannot be a function expression
+        const title = $(this).text()
+        const url = $(this).find('a').attr('href')
+        articles.push({
+          title,
+          url
+        })
+      })
+      res.json(articles)
+    }).catch(err => console.log(err))
 
 })
+
+//import config from './src/config.json'
+const config = require('./src/config.json')
+
+var connection = mysql.createConnection({
+  host     : config.database.host,
+  user     : config.database.user,
+  password : config.database.password
+});
+
+connection.connect();
+
+//import urls from './urls.json'
+const urls = require('./src/urls.json')
+
+// running function for every entry in urls
+// define function 
+function getArticles() {
+  urls.forEach(url => {
+    axios(url)
+      .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+        $('h1', html).each(function () { //<-- cannot be a function expression
+          const title = $(this).text()
+          const url = $(this).find('a').attr('href')
+          articles.push({
+            title,
+            url
+          })
+        })
+        console.log(articles)
+      }).catch(err => console.log(err))
+  })
+}
+// run function every 6 hours
+setInterval(() => {
+  getArticles()
+}, 21600000)
+
 
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
