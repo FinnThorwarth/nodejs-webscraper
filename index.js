2842c153a1f175ad0e1663ce8a95b3cd4f3ca87e
Finn Thorwarth <finn@wg-werbeagentur.de>
2022-07-05 12:33:35 +0200

    Updated

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
-const url = 'https://www.wg-werbeagentur.de'
-
 
 app.get('/', function (req, res) {
   res.json('WG Webscraper')
 })
 
 app.get('/results', (req, res) => {
-  getArticles()
+  getWEData()
   res.json('WG Webscraper')
 })
 
@@ -29,9 +27,24 @@ var connection = mysql.createConnection({
   database: config.database.database
 });
 
+
+//handle disconnects
+connection.on('error', function (err) {
+  console.log('db error', err)
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    connection = mysql.createConnection({
+      host: config.database.host,
+      user: config.database.user,
+      password: config.database.password,
+      database: config.database.database
+    });
+  } else {
+    throw err;
+  }
+});
+
 // safe to database
 function safeToDatabase(data) {
-  connection.connect();
   // check if data is already in database
   connection.query('SELECT * FROM Projekte WHERE Name = ' + mysql.escape(data.name), function (err, result) {
     if (err) throw err;
@@ -39,14 +52,22 @@ function safeToDatabase(data) {
       // insert data into database with mysql
       connection.query('INSERT INTO Projekte (Name,URL) VALUES (' + mysql.escape(data.name) + ',' + mysql.escape(data.url) + ')', function (err, result) {
         if (err) throw err;
-        console.log('1 record inserted');
+        console.log('Eintrag erstellt');
       });
     } else {
-      console.log('data already in database')
+      console.log('Eintrag vorhanden')
+      // get id from the result object
+      var id = result[0].ID;
+
+      // write versions to database
+      connection.query('INSERT INTO Resultate (Projekt_ID,Result,WE,PHP,SQLVersion) VALUES (' + id + ',' + mysql.escape(JSON.stringify(data.versions)) + ',' + mysql.escape(data.versions.version) + ',' + mysql.escape(data.versions.phpVersion) + ',' + mysql.escape(data.versions.sqlVersion) + ')', function (err, result) {
+        if (err) throw err;
+        console.log('Eintrag geschrieben');
+      });
+
     }
   });
   // close connection
-  connection.end();
 }
 
 //import urls from './urls.json'
@@ -54,20 +75,19 @@ const urls = require('./src/urls.json')
 
 // running function for every entry in urls
 // define function 
-function getArticles() {
-  urls.forEach(url => {
-    axios(url)
+function getWEData() {
+  urls.forEach(urlData => {
+    axios(urlData.urlAPI)
       .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
+        const json = response.data
         const data = {}
-        $('h1', html).each(function () { //<-- cannot be a function expression
-          data.title = $(this).text()
-          data.name = url.name
-          data.url = url.url
-        })
-        console.log(data)
-        console.log(url.name)
+
+        // Daten definieren
+        data.name = urlData.name
+        data.url = urlData.url
+        data.urlAPI = urlData.urlAPI
+        data.versions = json
+
         // safe to database
         safeToDatabase(data);
 
@@ -75,10 +95,34 @@ function getArticles() {
   })
 }
 
+
+function getWEIncludes() {
+  urls.forEach(urlData => {
+    axios(urlData.urlAPI)
+      .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const data = {}
+        $('h1', html).each(function () { //<-- cannot be a function expression
+          data.title = $(this).text()
+          data.name = urlData.name
+          data.url = urlData.url
+          data.urlAPI = urlData.urlAPI
+        })
+        console.log(data)
+        console.log(urlData.name)
+        // safe to database
+        safeToDatabase(data);
+
+      }).catch(err => console.log(err))
+  })
+}
+
+
 // run function every 6 hours
 setInterval(() => {
   getArticles()
-}, 21600000)
+}, 3600000)
 
 
 
